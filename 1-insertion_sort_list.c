a8ab2ff9e3664ea4ea1585c43d626c798d1da625
Mariam22MR <xxxsaakk@gmail.com>
2024-01-22 12:27:46 -0800

    a

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Insertion sort doubly linked list
+ * @list: address of pointer to head node.
+ *
+ * Return: NULL.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *temp;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	i = (*list)->next;
+
+	while (i != NULL)
+	{
+		temp = i;
+
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			temp->prev->next = temp->next;
+
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+
+			temp->next = temp->prev;
+			temp->prev = temp->prev->prev;
+			temp->next->prev = temp;
+
+			if (temp->prev == NULL)
+				*list = temp;
+
+			print_list(*list);
+		}
+
+		i = i->next;
+	}
+}
