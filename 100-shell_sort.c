1feeaa39c4697566beabf987d7391a100fb76242
Mariam22MR <xxxsaakk@gmail.com>
2024-01-27 11:54:33 -0800

    a

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -22,7 +22,7 @@ void shell_sort(int *array, size_t size)
 	{
 		for (i = gap; i < size; i++)
 		{
-			tmp = array[i];
+			temp = array[i];
 			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 				array[j] = array[j - gap];
 			array[j] = temp;
