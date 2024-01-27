1d9ebfee84a5291ba53e708833d87565dccd503e
Mariam22MR <xxxsaakk@gmail.com>
2024-01-27 11:53:17 -0800

    a

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,33 +1,33 @@
 #include "sort.h"
 
 /**
- * shell_sort - Sort array of ints using Shell sort.
- * @array: integer array to sort.
+ * shell_sort - Sort array of integers using Shell sort.
+ * @array: integer array of sort.
  * @size: size of array.
  *
- * Return: voide.
+ * Return: void.
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int temp;
+	size_t gap = 1, i, j;
+	int temp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-    while (gap >= 1)
-    {
-        for (i = gap; i < size; i++)
-        {
-		temp = array[i];
-		for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
-                array[j] = array[j - gap];
-            array[j] = temp;
-        }
-        gap = (gap - 1) / 3;
-        print_array(array, size);
-    }
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
+				array[j] = array[j - gap];
+			array[j] = temp;
+		}
+		gap = (gap - 1) / 3;
+		print_array(array, size);
+	}
 }
