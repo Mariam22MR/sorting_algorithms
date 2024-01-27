affd3194c8963038724530d5de964073c363efa2
Mariam22MR <xxxsaakk@gmail.com>
2024-01-27 12:18:25 -0800

    a

---
+++ 102-counting_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+
+/**
+ * counting_sort - Sort array of integers using counting sort algorithm.
+ * @array: intrger array to sort.
+ * @size: size of array.
+ **/
+void counting_sort(int *array, size_t size)
