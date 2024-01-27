43e9662e0060d02392c2aa14aa933249f5f9ecbe
Mariam22MR <xxxsaakk@gmail.com>
2024-01-27 13:03:22 -0800

    a

--- sort.h
+++ sort.h
@@ -37,20 +37,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quicksort(int *array, size_t size, size_t, size_t);
 
-void bubble_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void merge_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void quick_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-
 
 #endif
